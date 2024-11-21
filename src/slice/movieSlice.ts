7e0b852d415ef9ed4eae72e0c385a75dd7c8da6a
Sabarinath P S <sabarinathps@Sabarinaths-MacBook-Pro-2.local>
2024-11-21 13:44:56 +0400

    removed unwanted code

--- src/slice/movieSlice.ts
+++ src/slice/movieSlice.ts
@@ -33,7 +33,6 @@ export const movieSlice = createSlice({
       })
       .addCase(fetchMoviesByTitle.fulfilled, (state, action) => {
         state.isMoviesListLoading = false;
-        console.log("payyload: ", action.payload);
         state.movies = [action.payload];
       })
       .addCase(fetchMoviesByTitle.rejected, (state) => {
@@ -48,41 +47,15 @@ export const fetchMoviesByTitle = createAsyncThunk(
   "movies/byTitle",
   async (title: string, thunkAPI) => {
     try {
-      //   const res = await fetch(
-      `https://www.omdbapi.com/?apikey=d3bd536a&t=${title}`;
-      //   {
-      //     method: "GET",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //       "Access-Control-Allow-Origin": "*",
-      //       "Access-Control-Allow-Credentials": "true",
-      //       "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-      //       "Access-Control-Allow-Headers":
-      //         "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-      //     },
-      //     mode: "no-cors",
-      //   }
-      //   );
-
       return fetch(`https://www.omdbapi.com/?apikey=d3bd536a&t=${title}`)
-        .then(
-          (resp) => resp.json() // this returns a promise
-        )
+        .then((resp) => resp.json())
         .then((repos) => {
-          console.log(repos);
           return repos;
         })
         .catch((ex) => {
           console.error(ex);
         });
-
-      //   if (!res.ok) {
-      //     return thunkAPI.rejectWithValue(new Error(res.statusText));
-      //   }
-
-      //   return res.json();
     } catch (err) {
-      console.error("Error occured: ", err);
       return thunkAPI.rejectWithValue(err);
     }
   }
